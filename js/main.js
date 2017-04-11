23ea054a72e445147cc443d2298919b08e2ed686
Nallely House <nallelyht@gmail.com>
2017-04-11 11:32:08 -0500

    funcionalidad js falta cancha tennis

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+var botonFut= document.getElementById('fut');
+var botonBasket =document.getElementById('basket');
+var botonTen = document.getElementById('ten')
+
+botonFut.addEventListener("click",mostrarFut);
+botonBasket.addEventListener("click", mostrarBasket);
+botonTen.addEventListener("click", mostrarTennis);
+
+function mostrarFut(){
+	document.getElementById('futball').style.display = "block";
+	document.getElementById('basketball').style.display = "none";
+	document.getElementById('tennis').style.display = "none";
+}
+function mostrarBasket(){
+	document.getElementById('basketball').style.display = "block";
+	document.getElementById('futball').style.display = "none";
+	document.getElementById('tennis').style.display = "none";
+}
+function mostrarTennis(){
+	document.getElementById('tennis').style.display = "block";
+	document.getElementById('basketball').style.display = "none";
+	document.getElementById('futball').style.display = "none";
+}
